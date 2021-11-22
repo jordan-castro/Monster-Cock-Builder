12f93d590a376730c5f26d6bf09fc2fda0607413
James Garfield <jordan@grupojvm.com>
2021-11-22 12:27:14 -0500

    gen/monster/gen_methods: Format the name if passed

--- src/gen/monster/gen_methods.rs
+++ src/gen/monster/gen_methods.rs
@@ -9,7 +9,7 @@ impl MonsterCock {
 
     pub fn with_name(id: u32, generation: i32, cock_type: CockType, name: String, is_test_net: bool) -> MonsterCock {
         let mut monster = Self::base(id, generation, cock_type, String::new(), None, is_test_net);
-        monster.name = name;
+        monster.name = Self::format_name(name, id);
         monster
     }
 
@@ -19,11 +19,15 @@ impl MonsterCock {
 
     pub fn with_name_and_category(id: u32, generation: i32, cock_type: CockType, name: String, category: String, is_test_net: bool) -> MonsterCock {
         let mut monster = Self::base(id, generation, cock_type, category, None, is_test_net);
-        monster.name = name;
+        monster.name = Self::format_name(name, id);
         monster
     }
 
     pub fn with_attributes(id: u32, generation: i32, cock_type: CockType, attributes: Vec<CockTribute>, is_test_net: bool) -> MonsterCock {
         Self::base(id, generation, cock_type, String::new(), Some(attributes), is_test_net)
+    }
+
+    fn format_name(name: String, id: u32) -> String {
+        format!("{} #{}", name, id)
     }
 }
