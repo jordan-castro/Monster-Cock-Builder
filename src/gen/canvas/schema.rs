1785df5cfa94d7ae3b5c3a8f24adc7e4bde1f72e
James Garfield <jordan@grupojvm.com>
2021-11-23 00:19:34 -0500

    gen/canvas/schema: Some logic to decide the schema size

--- src/gen/canvas/schema.rs
+++ src/gen/canvas/schema.rs
@@ -1,8 +1,11 @@
-use crate::{gen::types::SchemaSkipType, utils::randomify::random_skip_values};
+use crate::{
+    gen::types::SchemaSkipType,
+    utils::randomify::{random_skip_type, random_skip_values},
+};
 use itertools::Itertools;
 use rand::Rng;
 
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Clone, PartialEq)]
 pub struct Schema {
     pub title: String,
     pub modulus: i32,
@@ -19,13 +22,21 @@ impl Schema {
         skips: Vec<i32>,
         skip_type: SchemaSkipType,
     ) -> Self {
-        Self {
+        let mut schema = Schema {
             title,
             modulus,
             size,
             skips,
             skip_type,
+        };
+        match schema.skip_type {
+            SchemaSkipType::Original => {}
+            SchemaSkipType::V2 => {
+                schema.size = schema.modulus;
+            }
         }
+
+        schema
     }
 
     pub fn stringify_skips(&self) -> String {
@@ -34,18 +45,18 @@ impl Schema {
 
     pub fn random_schema(title: String) -> Self {
         let modulus = get_modulus();
-        let size = get_draw_size(modulus);
-        Schema {
+        let size = get_draw_size();
+        Self::new(
             title,
             modulus,
             size,
-            skips: random_skip_values(),
-            skip_type: SchemaSkipType::Original,
-        }
+            random_skip_values(),
+            random_skip_type(),
+        )
     }
 
     /// Print to the screen the values of the draw.
-    pub fn debug_schema(&self) {
+    pub fn debug(&self) {
         println!("===============================");
         println!("{}", self.title);
         println!("Modulus: {}", self.modulus);
@@ -69,12 +80,7 @@ fn get_modulus() -> i32 {
     rand::thread_rng().gen_range(2..30)
 }
 
-///
 /// Get a size for the draw object.
-///
-/// # Returns
-/// `i32` The size of the draw.
-///
-fn get_draw_size(modulus: i32) -> i32 {
-    rand::thread_rng().gen_range(1..modulus + 20)
+fn get_draw_size() -> i32 {
+    rand::thread_rng().gen_range(1..100)
 }
