789d3a319cc8e593177aa54837d9165d7a0ddd6d
James Garfield <jordan@grupojvm.com>
2021-11-23 00:18:22 -0500

    scripts/populateschemas: create

---
+++ scripts/populateschemas.bat
@@ -0,0 +1,16 @@
+@echo OFF
+
+@REM This script is used to populate the schemas.txt data file.
+@REM It's not really necessary if you're using the default data.
+@REM It's here to help you if you want to add your own data.
+@REM Just change device to mckbuilder if alredy built. 
+
+set device=cargo run
+set loops=%1
+
+cd ../
+for /l %%x in (1, 1, %loops%) do (
+   %device% train 10
+    echo "Trained (%x% + 10) of %loops%"
+)
+cd scripts
